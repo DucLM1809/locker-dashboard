65446c6533eddac6f3738b64f3fb1647a8899635
minhduc <minhductxqt@gmail.com>
2023-04-11 08:05:19 +0700

    chore: config project

---
+++ src/api/axiosBaseQuery,.ts
@@ -0,0 +1,37 @@
+import type { BaseQueryFn } from '@reduxjs/toolkit/query';
+import type { AxiosRequestConfig, AxiosError } from 'axios';
+import axiosClient from './axiosClient';
+import { BASE_URL } from '@/constants/endpoints';
+
+const axiosBaseQuery =
+  (): BaseQueryFn<
+    {
+      url: string;
+      method: AxiosRequestConfig['method'];
+      data?: AxiosRequestConfig['data'];
+      params?: AxiosRequestConfig['params'];
+    },
+    unknown,
+    unknown
+  > =>
+  async ({ url, method, data, params }) => {
+    try {
+      const result = await axiosClient({
+        url: BASE_URL + url,
+        method,
+        data,
+        params,
+      });
+      return { data: result.data };
+    } catch (axiosError) {
+      let err = axiosError as AxiosError;
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      };
+    }
+  };
+
+export default axiosBaseQuery;
--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,14 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 import store from './stores';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './constants/theme';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root'),
 );
